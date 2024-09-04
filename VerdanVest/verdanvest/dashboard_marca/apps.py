028c42645a49f9140ccddde17b02f7fa3ed6ede2
Jaramillo <114198104+Jxtsy@users.noreply.github.com>
2024-09-04 14:32:08 -0600

    Initialized project

---
+++ VerdanVest/verdanvest/dashboard_marca/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DashboardMarcaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'dashboard_marca'
---
+++ VerdanVest/verdanvest/estandares_valoraciones/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EstandaresValoracionesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'estandares_valoraciones'
